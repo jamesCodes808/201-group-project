ce928b86b736b0ee39b1a1f7e810959d736c1e32
Ernesto Cardenas <71235388+Ernestocardenas@users.noreply.github.com>
2023-02-01 20:28:50 -0500

    Game logic running V3

--- js/hangman.js
+++ js/hangman.js
@@ -1,100 +1,156 @@
-'use strict'
+"use strict";
 
-var alphabet = ['a','A','b','B','c','C','d','D','e','E','f','F','g','G','h','H','i','I','j','J','k','K','l','L','m','M','n','N','o','O','p','P','q','Q','r','R','s','S','t','T','u','U','v','V','w','W','x','X','y','Y','z','Z'];
+var alphabet = [
+  "a",
+  "A",
+  "b",
+  "B",
+  "c",
+  "C",
+  "d",
+  "D",
+  "e",
+  "E",
+  "f",
+  "F",
+  "g",
+  "G",
+  "h",
+  "H",
+  "i",
+  "I",
+  "j",
+  "J",
+  "k",
+  "K",
+  "l",
+  "L",
+  "m",
+  "M",
+  "n",
+  "N",
+  "o",
+  "O",
+  "p",
+  "P",
+  "q",
+  "Q",
+  "r",
+  "R",
+  "s",
+  "S",
+  "t",
+  "T",
+  "u",
+  "U",
+  "v",
+  "V",
+  "w",
+  "W",
+  "x",
+  "X",
+  "y",
+  "Y",
+  "z",
+  "Z",
+];
 
 //potential words to guess
 var pokemon = ['bulbasaur', 'ivysaur', 'venusaur', 'charmander', 'charmeleon', 'charizard', 'squirtle', 'wartortle', 'blastoise', 'caterpie', 'metapod', 'butterfree', 'weedle', 'kakuna', 'beedrill', 'pidgey', 'pidgeotto', 'pidgeot', 'rattata', 'raticate', 'spearow', 'fearow', 'ekans', 'arbok', 'pikachu', 'raichu', 'sandshrew', 'sandslash', 'nidoran', 'nidorina', 'nidoqueen', 'nidorino', 'nidoking', 'clefairy', 'clefable', 'vulpix', 'ninetales', 'jigglypuff', 'wigglytuff', 'zubat', 'golbat', 'oddish', 'gloom', 'vileplume', 'paras', 'parasect', 'venonat', 'venomoth', 'diglett', 'dugtrio', 'meowth', 'persian', 'psyduck', 'golduck', 'mankey', 'primeape', 'growlithe', 'arcanine', 'poliwag', 'poliwhirl', 'poliwrath', 'abra', 'kadabra', 'alakazam', 'machop', 'machoke', 'machamp', 'bellsprout', 'weepinbell', 'victreebell', 'tentacool', 'tentacruel', 'geodude', 'graveler', 'golem', 'ponyta', 'rapidash', 'slowpoke', 'slowbro', 'magnemite', 'magneton', 'farfetchd', 'doduo', 'dodrio', 'seel', 'dewgong', 'grimer', 'muk', 'shellder', 'cloyster', 'gastly', 'haunter', 'gengar', 'onix', 'drowsee', 'hypno', 'krabby', 'kingler', 'voltorb', 'electrode', 'exeggcute', 'exeggutor', 'cubone', 'marowak', 'hitmonlee', 'hitmonchan', 'lickitung', 'koffing', 'weezing', 'rhyhorn', 'rhydon', 'chansey', 'tangela', 'kangaskhan', 'horsea', 'seadra', 'goldeen', 'seaking', 'staryu', 'starmie', 'mrmime', 'scyther', 'jynx', 'electabuzz', 'magmar', 'pinsir', 'tauros', 'magikarp', 'gyarados', 'lapras', 'ditto', 'eevee', 'vaporeon', 'jolteon', 'flareon', 'porygon', 'omanyte', 'omastar', 'kabuto', 'kabutops', 'aerodactyl', 'snorlax', 'articuno', 'zapdos', 'moltres', 'dratini', 'dragonair', 'dragonite', 'mewtwo', 'mew'];
+
 
 var guesses = 10;
 var wins = 0;
 var losses = 0;
 
-var answer = pokemon[Math.floor(Math.random()*pokemon.length)];
+var answer = pokemon[Math.floor(Math.random() * pokemon.length)];
 
 //create '_' placeholders in HTML document with the same number of characters as the pokemon
 var answerLength = answer.length;
 
-var underscore = Array.from('_'.repeat(answerLength));
+var underscore = Array.from("_".repeat(answerLength));
 
 //initializes an array to store letter guesses
 var letterGuess = [];
 
 //initializes text on the window load
-window.onload = function(){
+window.onload = function () {
+  document.getElementById("answer").innerHTML = underscore.join(" ");
 
-    
-    document.getElementById("answer").innerHTML = underscore.join(' ');
+  document.getElementById("guessCount").innerHTML = "Guesses left: " + guesses;
 
-    document.getElementById("guessCount").innerHTML = "Guesses left: " + guesses;
+  document.getElementById("lettersGuessed").innerHTML =
+    "Letters guessed: " + letterGuess;
+};
 
-    document.getElementById("lettersGuessed").innerHTML = "Letters guessed: " + letterGuess;
+//function runs after click me
+document.getElementById("startGame").onclick = function () {
+  document.getElementById("pokemonimg").src =
+    "assets/img/pokemonName/" + answer + ".png";
 
-}
-
-//function runs after click me 
-document.getElementById("startGame").onclick = function(event) {
-
-
-    document.getElementById('pokemonimg').src = "assets/img/pokemonName/"+answer+".png";
-
-    if (guesses === 0 || underscore.join('') === answer){
+  if (guesses === 0 || underscore.join("") === answer) {
     //    prevents guessing after game end
-    }
-    else {
-        //userGuess is recorded based on key pressed
-        // var userGuess = event.key;
+  } else {
+    let userGuess = "";
+    document.addEventListener("keydown", (event) => {
+      event.preventDefault();
+      userGuess = event.key;
+      console.log(event);
+      console.log(userGuess);
 
+      let userGuessLower = userGuess.toLowerCase();
 
-        // test add
-        // console.log(userGuess);
+      console.log(userGuess);
 
-        let userGuess = '';
-    document.addEventListener('keydown',(event) => {
-        event.preventDefault();
-        userGuess = event.key;
-        console.log(event);
-        console.log(userGuess);
-    
-        
-    });
+      if (alphabet.includes(userGuessLower)) {
+        if (answer.indexOf(userGuessLower) < 0) {
+          if (letterGuess.indexOf(userGuessLower) < 0) {
+            guesses = guesses - 1;
 
-        if (alphabet.indexOf(userGuess) > -1) {
-
-            userGuessLower = userGuess.toLowerCase();
-
-            if (answer.indexOf(userGuessLower) < 0) {
-
-                if (letterGuess.indexOf(userGuessLower) < 0) {
-
-                    guesses = guesses - 1;
-
-                    document.getElementById("guessCount").innerHTML = "Guesses left: " + guesses;
-                }
-            }
-
-            //appends userGuess to lettersGuessed array and updates document
-            if (letterGuess.indexOf(userGuessLower) < 0) {
-                letterGuess.push(userGuessLower);
-
-                document.getElementById("lettersGuessed").innerHTML = "Letters guessed: " + letterGuess;
-            }
-
-            //checks to see if the guess is part of the answer
-            if (answer.indexOf(userGuessLower) > -1) {
-
-                //cycles through the answer to find locations matching the user guess
-                for (var i = 0; i < answer.length; i++) {
-
-                    if (answer[i] === userGuessLower) {
-
-                        //updates underscore with letter at correct location
-                        underscore[i] = userGuessLower;
-
-                        document.getElementById("answer").innerHTML = underscore.join(' ');
-                    }
-                }
-            }
+            document.getElementById("guessCount").innerHTML =
+              "Guesses left: " + guesses;
+          }
         }
-    }
 
-}
+        //appends userGuess to lettersGuessed array and updates document
+        if (letterGuess.indexOf(userGuessLower) < 0) {
+          letterGuess.push(userGuessLower);
+
+          document.getElementById("lettersGuessed").innerHTML =
+            "Letters guessed: " + letterGuess;
+        }
+
+        //checks to see if the guess is part of the answer
+        if (answer.indexOf(userGuessLower) > -1) {
+          //cycles through the answer to find locations matching the user guess
+          for (var i = 0; i < answer.length; i++) {
+            if (answer[i] === userGuessLower) {
+              //updates underscore with letter at correct location
+              underscore[i] = userGuessLower;
+
+              document.getElementById("answer").innerHTML =
+                underscore.join(" ");
+
+            }
+          }
+        }
+      }
+    });
+  }
+
+  if (underscore.join('') === answer) {
+    wins = wins + 1;
+
+    document.getElementById("wins").innerHTML = "Wins: " + wins;
+
+  }
+
+  if (guesses === 0) {
+    losses = losses + 1;
+
+    document.getElementById("losses").innerHTML = "Losses: " + losses;
+
+  }
+};
+
+
